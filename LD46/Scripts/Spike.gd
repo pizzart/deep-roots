aa524c38113a0d0c73970a6099543cea7a6005fa
TeaWhyDee <daniel.555365@gmail.com>
2020-04-18 17:39:29 +0300

    spikes

--- LD46/Scripts/Spike.gd
+++ LD46/Scripts/Spike.gd
@@ -5,5 +5,5 @@ func _ready():
 
 
 func _on_Spike_body_entered(body):
-	if body.name = "Player":
+	if body.name == "Player":
 		pass
--- LD46/Scripts/TestLevel.gd
+++ LD46/Scripts/TestLevel.gd
@@ -8,7 +8,7 @@ func _physics_process(delta):
 	if ($Player.position.x < -50):
 		$Spikes1.visible = true
 		for spike in $Spikes1.get_children():
-			spike.frame = 1;
+			spike.frame = 0;
 
 
 
