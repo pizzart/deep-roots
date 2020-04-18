7e2de18797b894c993b5d2515d4c78d1b4b0e1fc
PizzArt <46490176+PizzArt@users.noreply.github.com>
2020-04-18 12:12:51 +0300

    Add files via upload

---
+++ LD46/Scripts/AudioManager.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var rng = RandomNumberGenerator.new()
+var root = null
+
+func _ready():
+	root = get_tree().get_root()
+
+func _process(_delta):
+	rng.randomize()
+
+func play_sound(sound, volume, bus = "SFX"):
+	var soundPath = "res://Sounds" + sound
+	var stream = load(soundPath)
+	var player = AudioStreamPlayer.new()
+	
+	root.add_child(player)
+	
+	player.name = "Sound-" + str(rng.randi())
+	player.set_owner(root)
+	player.stream = stream
+	player.bus = bus
+	player.set_volume_db(volume)
+	player.play()
