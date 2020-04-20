c7561b47c1ddeaa2623487bb9bc819103c1050be
TeaWhyDee <daniel.555365@gmail.com>
2020-04-20 15:04:55 +0300

    level

--- LD46/Scripts/TestLevel.gd
+++ LD46/Scripts/TestLevel.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var story = [
 ["Pr", "lalala, what a normal day"],
+["Pr", "How weird, an actual plant in this empty le- land"],
 ["Pr", "I've fallen and I can't get up!", "Pt", "You're up", "Pr", "I don't have a lying animation", "Pr", "Anyway, who are you?", "Pt", "I'm a- I'm a talking plant", "Pr", "cool", "Pt", "Ok so you have to help me get out of here", "Pt", "Bring me the minerals, see, here's Nitrogen", "Pt", "And be quick! You have to KEEP ME ALIVE "], 
 ["Pr", "gam", "Pt", "ing"]]
 var progress = 0;
@@ -9,7 +10,10 @@ var i = 0;
 signal dialogue
 signal customDialogue
 
+var earthquake = false
+
 func dialogue():
+	$Player.motion.x = 0
 	var pos = story[progress][i]
 	var d = story[progress][i+1]
 	if (pos == "Pr"):
@@ -34,8 +38,17 @@ func _on_Dialogue_done():
 		progress +=1;
 		$Player.canMove = true;
 		
+	if (earthquake):
+		earthquake()
+		
+
+func earthquake():
+	
+
+
 func _on_Dialogue_simple_done():
 	$Player.canMove = true;
+
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
@@ -54,10 +67,14 @@ func _on_Area2D2_body_entered(body):
 
 func _on_GoBack_body_entered(body):
 	if body.name == "Player":
-		customDialogue("Bo back. The game is to the left")
+		customDialogue("Go back. The game is to the left")
+		$Triggers/GoBack.queue_free()
 
 
-
-
+func _on_OW_Plant_body_entered(body):
+	if body.name == "Player":
+		dialogue()
+		$Triggers/OW_Plant.queue_free()
+		earthquake = true
 
 
